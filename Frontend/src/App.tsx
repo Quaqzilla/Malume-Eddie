d249f584c44cf91b6f78680bb2a1f0c1a72f59ea
Phemelo Mokotedi <bbrigett207@gmail.com>
2025-11-15 23:11:22 +0200

    router

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css'
 import { Home } from './Pages/Home/Home'
+import {BrowserRouter, Route, Routes} from "react-router-dom"
 
 function App() {
 
   return (
-    <div>
-      <Home/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
